cfd4444c4604430a605087aebd5133f22a285c44
your_name <orkunkorcak@gmail.com>
2025-07-22 23:05:06 +0300

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { startServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
 
 const initStart = async () => {
   try {
-   
+   await initMongoConnection();
     startServer();
   } catch (error) {
     console.error('Failed to initialize MongoDB connection:', error);
