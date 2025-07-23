b2114de7069a78495d07e1dce3d2a280ada7ed63
your_name <orkunkorcak@gmail.com>
2025-07-24 01:00:33 +0300

    middlewares eklendi swagger düzenleme yapıldı

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 
 const PORT = Number(env('PORT', '3000'));
@@ -20,7 +22,9 @@ export const startServer = () => {
       });
     });
     
+  app.use(notFoundHandler);
 
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
