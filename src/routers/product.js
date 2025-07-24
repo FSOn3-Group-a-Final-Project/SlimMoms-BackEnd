106232f440f8509516d76ff9b6fb0d15af0e308e
Serhat GENÇ <gncsrht05@gmail.com>
2025-07-24 16:21:56 +0300

    product.js dosyasındaki değişiklikleri kaydettim

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {getProducts} from '../controllers/product.js';
+import {getFilteredProducts} from '../controllers/product.js';
 
 const router = express.Router();
 
 //Query string ile ürün arama route'u
-router.get('/', getProducts);
+router.get('/', getFilteredProducts);
 
 export default router;
