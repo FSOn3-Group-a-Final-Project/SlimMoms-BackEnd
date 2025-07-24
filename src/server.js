1ff989bd78afd641e061e4b50f10aa81a4c1b403
your_name <orkunkorcak@gmail.com>
2025-07-24 18:56:50 +0300

    debug solvet

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import calcRoutes from './routers/calcRoutes.js';
 import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-import productsRoutes from './routers/product.js';
+// import productsRoutes from './routers/product.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -19,22 +19,21 @@ export const startServer = () => {
   );
   app.use(cors());
   app.use('/api', calcRoutes); // hesapalama routu
- // app.use('/api/products', productsRoutes); // arama routu'u
+  // app.use('/api/products', productsRoutes); // arama routu'u
 
-  app.get('/'), (req, res) => {
+  app.get('/', (req, res) => {
     res.json({
       message: 'Welcome to the Slim Moms',
     });
+  });
 
-    
-    app.use(router);
+  app.use(router);
 
-    app.use(notFoundHandler);
+  app.use(notFoundHandler);
 
-    app.use(errorHandler);
+  app.use(errorHandler);
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  };
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
