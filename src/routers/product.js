1612a4e30f7fbe5bcd6d68b695e4eec53e05e6ab
Yagiz Berk <berkyagizkaran@gmail.com>
2025-07-25 20:18:11 +0300

    Swagger documentation

--- src/routers/product.js
+++ src/routers/product.js
@@ -4,6 +4,7 @@ import {
   addProductToDiary,
   removeProductFromDiary,
   getDiaryByDate,
+  getAllDiaryProducts,
 } from '../controllers/product.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
@@ -11,8 +12,9 @@ const router = express.Router();
 
 //Query string ile ürün arama route'u
 router.get('/', getFilteredProducts);
+router.get('/diary', authenticate, getAllDiaryProducts);
 
 router.post('/', authenticate, addProductToDiary);
-router.delete('/:productId', authenticate, removeProductFromDiary);
-router.get('/diary/:date', authenticate, getDiaryByDate); // router.get('/', authenticate, getDiaryByDate);
+router.delete('/diary/:date/:productId', authenticate, removeProductFromDiary);
+router.get('/diary/:date', authenticate, getDiaryByDate);
 export default router;
